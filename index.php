7ba96267a5af0c2de756b0fa1ce46e9a08354e03
Surfoo <surfooo@gmail.com>
2013-06-27 22:48:07 +0200

    array(), for PHP < 5.4

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ session_start();
 spl_autoload_register(function ($classname) {
     $classname = ltrim($classname, "\\");
     preg_match('/^(.+)?([^\\\\]+)$/U', $classname, $match);
-    $classname = str_replace("\\", "/", $match[1]). str_replace(["\\", "_"], "/", $match[2]) . ".php";
+    $classname = str_replace("\\", "/", $match[1]). str_replace(array("\\", "_"), "/", $match[2]) . ".php";
     include_once __DIR__ . '/src/' . $classname;
 });
 
