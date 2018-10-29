3d2a87f08bcaeb1fd291f0757d60829f0eeec3b8
Surfoo <surfooo@gmail.com>
2018-10-29 23:12:28 +0100

    Added context on exception

--- src/Exception/GeocachingSdkException.php
+++ src/Exception/GeocachingSdkException.php
@@ -4,13 +4,24 @@ namespace Geocaching\Exception;
 
 class GeocachingSdkException extends \Exception
 {
+
+    protected $context = null;
+
     /**
-     * GeocachingSdkException constructor.
-     *
      * @param string $message
+     * @param int    $code
      */
-    public function __construct($message = 'Bad request exception', $code = 0)
+    public function __construct(string $message = 'Bad request exception', int $code = 0, array $context = [])
     {
         parent::__construct($message, $code);
+
+        $this->context = $context;
+    }
+
+    /**
+     * @return array
+     */
+    public function getContext() {
+        return $this->context;
     }
 }
