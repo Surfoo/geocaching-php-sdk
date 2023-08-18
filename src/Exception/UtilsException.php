8918f6a574a7018b35d9a96cd7c7ea824cf06b81
Surfoo <surfooo@gmail.com>
2023-08-18 23:06:10 +0200

    not used

--- src/Exception/UtilsException.php
+++ src/Exception/UtilsException.php
@@ -4,15 +4,8 @@ namespace Geocaching\Exception;
 
 class UtilsException extends \Exception
 {
-    public function __construct(string $message, int $code = 0, protected ?array $context = [])
+    public function __construct(string $message, int $code = 0)
     {
         parent::__construct($message, $code);
-
-        $this->context = $context;
-    }
-
-    public function getContext(): array
-    {
-        return $this->context;
     }
 }
