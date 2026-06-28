bc4aae93476e566f358d72b59097ab077b990239
Surfoo <surfooo@gmail.com>
2026-06-29 00:04:41 +0200

    Added getContext method

--- src/Exception/UtilsException.php
+++ src/Exception/UtilsException.php
@@ -4,8 +4,13 @@ namespace Geocaching\Exception;
 
 class UtilsException extends \Exception
 {
-    public function __construct(string $message, int $code = 0)
+    public function __construct(string $message, int $code = 0, private readonly array $context = [])
     {
         parent::__construct($message, $code);
+    }
+
+    public function getContext(): array
+    {
+        return $this->context;
     }
 }
